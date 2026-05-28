096d13725cfe3934c83fca09e5745538f8b8d094
LoreMoli22 <lore82laplata@gmail.com>
2026-05-27 23:28:07 -0300

    configuracion panel Paciente y Profesional

--- turnos/admin.py
+++ turnos/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import Paciente, Profesional
+
+###### Registramos los modelos para que aparezcan en el panel de administración
+admin.site.register(Paciente)
+admin.site.register(Profesional)
