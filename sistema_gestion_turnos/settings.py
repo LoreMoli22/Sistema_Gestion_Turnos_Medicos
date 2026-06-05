e34bd50588e3fc231f2a6245fffe54e91a98d5f1
LoreMoli22 <lore82laplata@gmail.com>
2026-06-05 17:37:12 -0300

    Configuro PostgreSQL para produccion

--- sistema_gestion_turnos/settings.py
+++ sistema_gestion_turnos/settings.py
@@ -97,13 +97,13 @@ WSGI_APPLICATION = 'sistema_gestion_turnos.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+import dj_database_url
 
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL', f'sqlite:///{BASE_DIR}/db.sqlite3')
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
