ab54c30ce8d290ec3b2db4a9d3bff5d64fefa681
LoreMoli22 <lore82laplata@gmail.com>
2026-06-12 01:41:07 -0300

    correcciones settings.py

--- sistema_gestion_turnos/settings.py
+++ sistema_gestion_turnos/settings.py
@@ -145,7 +145,9 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_PORT = 465 
+EMAIL_USE_TLS = False
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_USE_SSL= True
+EMAIL_TIMEOUT = 10
