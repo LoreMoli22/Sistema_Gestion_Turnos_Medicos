a34a1409ef9553f7f9ac3e0f22a1d0d9ec3c590d
LoreMoli22 <lore82laplata@gmail.com>
2026-06-12 00:11:54 -0300

    email oculto

--- sistema_gestion_turnos/settings.py
+++ sistema_gestion_turnos/settings.py
@@ -147,5 +147,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'lore22laplata@gmail.com'
-EMAIL_HOST_PASSWORD = 'gxwy jdmx zqps kpmh'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
